7fde868fcb84a389c5dd8c97476d7779f56a0fd5
Joel Eche <joel.eche5@hotmail.com>
2016-11-26 19:14:27 -0500

    Registro de mascota - Formularios y vistas basados en funciones

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -4,6 +4,8 @@ from apps.adopcion.models import Persona
 
 class Vacuna(models.Model):
     nombre=models.CharField(max_length=50)
+    def __str__(self):
+        return '{}'.format(self.nombre)
 
 class Mascota(models.Model):
     nombre=models.CharField(max_length=50)
@@ -12,3 +14,6 @@ class Mascota(models.Model):
     fecha_rescate=models.DateField()
     persona=models.ForeignKey(Persona,null=True,blank=True,on_delete=models.CASCADE)
     vacuna=models.ManyToManyField(Vacuna,blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
